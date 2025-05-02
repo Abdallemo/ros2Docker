c17cd8facaf4f4918e507af8cbd8a814bc80e837
Abdalle mohamed <learn3038it@gmail.com>
2025-05-02 21:10:39 +0800

    ui update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,6 +1,6 @@
 $ErrorActionPreference = "Stop"
 
-Write-Host "📥 Downloading ros2docker script..."
+Write-Host " [*] Downloading ros2docker script..."
 
 $installDir = "$env:USERPROFILE\.config\.ros2docker"
 $confDir = "$env:USERPROFILE\.config"
@@ -19,7 +19,7 @@ Invoke-WebRequest -Uri "https://raw.githubusercontent.com/xaatim/ROS2-Docker-Lau
 Set-Content -Path $shortcutPath -Value "@echo off`nPowerShell -ExecutionPolicy Bypass -File `"$targetPath`" %*"
 
 if (-not ($env:PATH -split ";" | Where-Object { $_ -eq $confDir })) {
-    Write-Host "🧩 Adding $confDir to user PATH"
+    Write-Host " [>] Adding $confDir to user PATH"
     $currentPath = [Environment]::GetEnvironmentVariable("Path", "User")
     [Environment]::SetEnvironmentVariable("Path", "$currentPath;$confDir", "User")
 }
@@ -27,5 +27,5 @@ if (-not ($env:PATH -split ";" | Where-Object { $_ -eq $confDir })) {
 [Environment]::SetEnvironmentVariable("DISPLAY", "host.docker.internal:0.0", "User")
 [Environment]::SetEnvironmentVariable("XAUTHORITY", "/tmp/.docker.xauth", "User")
 
-Write-Host "✅ ros2docker installed successfully!"
-Write-Host "🔄 Restart your shell or log out/in to use 'ros2docker'"
+Write-Host " [ok] ros2docker installed successfully!"
+Write-Host " [*] Restart your shell or log out/in to use 'ros2docker'"
