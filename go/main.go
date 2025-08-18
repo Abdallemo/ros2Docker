5d03fe3d0d88aa86154541cb2930b7b102a8409e
EngCamo <learn3038it@gmail.com>
2025-08-18 17:47:47 +0800

    added execute

--- go/main.go
+++ go/main.go
@@ -6,6 +6,10 @@ package main
 import "github.com/Abdallemo/ros2Docker/cmd"
 
 func main() {
+	// _, err := utils.NewConfig("ros2")
+	// if err != nil {
+	// 	log.Fatal("couldne create config folder", err.Error())
+	// }
 	cmd.Execute()
 
 }
