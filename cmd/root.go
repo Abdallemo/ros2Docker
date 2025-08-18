1d0b536e64444aa3d1bda9dc1c64cdd851cabc7f
EngCamo <learn3038it@gmail.com>
2025-08-19 00:35:53 +0800

    moved go to the root

---
+++ cmd/root.go
@@ -0,0 +1,55 @@
+/*
+Copyright © 2025 Abdallemo <learn3038it@gmail.com>
+*/
+package cmd
+
+import (
+	"os"
+
+	"github.com/Abdallemo/ros2Docker/internals/docker"
+	"github.com/Abdallemo/ros2Docker/internals/utils"
+	"github.com/spf13/cobra"
+)
+
+// Docker a global intilized docker struct
+var (
+	dockerClient *docker.Docker
+	globalCfg    utils.GlbalConfig
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "ros2docker",
+	Short: "ros2docker is a lightweight CLI tool for managing ROS 2 development environments ",
+	Long: `ros2docker is a CLI tool that makes ROS 2 available across platforms 
+without requiring specific Linux distributions or complex setup. 
+It uses Docker under the hood to provide a consistent, portable ROS 2 
+environment that works seamlessly on Linux, macOS, and Windows.
+usa`,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	// Run: func(cmd *cobra.Command, args []string) { },
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute(d *docker.Docker, g utils.GlbalConfig) {
+	dockerClient = d
+	globalCfg = g
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.ros2Docker.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+
+}
