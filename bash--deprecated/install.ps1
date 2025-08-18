7724a03fb1d870bb57d55c713074e185904978df
EngCamo <learn3038it@gmail.com>
2025-08-19 00:31:43 +0800

    moved  --deprecated

---
+++ bash--deprecated/install.ps1
@@ -0,0 +1,29 @@
+$ErrorActionPreference = "Stop"
+
+Write-Host " [*] Downloading ros2docker script..."
+
+$installDir = "$env:USERPROFILE\.config\.ros2docker"
+$confDir = "$env:USERPROFILE\.config"
+$executableName = "ros2dockr.ps1"
+$targetPath = Join-Path $installDir $executableName
+$shortcutPath = Join-Path $confDir "ros2docker.cmd"
+$baseUrl = "https://raw.githubusercontent.com/xaatim/ROS2-Docker-Launcher/main/bash--deprecated/src"
+
+New-Item -ItemType Directory -Force -Path $installDir | Out-Null
+New-Item -ItemType Directory -Force -Path $confDir | Out-Null
+
+Invoke-WebRequest -Uri "$baseUrl/$executableName " -OutFile $targetPath
+
+Set-Content -Path $shortcutPath -Value "@echo off`nPowerShell -ExecutionPolicy Bypass -File `"$targetPath`" %*"
+
+if (-not ($env:PATH -split ";" | Where-Object { $_ -eq $confDir })) {
+    Write-Host " [>] Adding $confDir to user PATH"
+    $currentPath = [Environment]::GetEnvironmentVariable("Path", "User")
+    [Environment]::SetEnvironmentVariable("Path", "$currentPath;$confDir", "User")
+}
+
+[Environment]::SetEnvironmentVariable("DISPLAY", "host.docker.internal:0.0", "User")
+[Environment]::SetEnvironmentVariable("XAUTHORITY", "/tmp/.docker.xauth", "User")
+
+Write-Host " [ok] ros2docker installed successfully!"
+Write-Host " [*] Restart your shell or log out/in to use 'ros2dockr'"
