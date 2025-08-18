5488055dd11e38c7371c3ed87e6d027f418f0aa9
EngCamo <learn3038it@gmail.com>
2025-08-18 20:00:35 +0800

    add private docker instacne

--- go/cmd/root.go
+++ go/cmd/root.go
@@ -6,8 +6,12 @@ package cmd
 import (
 	"os"
 
+	"github.com/Abdallemo/ros2Docker/internals/docker"
 	"github.com/spf13/cobra"
 )
+
+// Docker a global intilized docker struct
+var dockerClient *docker.Docker
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
@@ -25,7 +29,8 @@ usa`,
 
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
-func Execute() {
+func Execute(d *docker.Docker) {
+	dockerClient = d
 	err := rootCmd.Execute()
 	if err != nil {
 		os.Exit(1)
