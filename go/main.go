3df1ab5fa5537ec2ee243cdc5b9cc3fb9169e41d
EngCamo <learn3038it@gmail.com>
2025-08-18 20:00:22 +0800

    utilized docker

--- go/main.go
+++ go/main.go
@@ -3,13 +3,27 @@ Copyright © 2025 Abdallemo <learn3038it@gmail.com>
 */
 package main
 
-import "github.com/Abdallemo/ros2Docker/cmd"
+import (
+	"log"
+
+	"github.com/Abdallemo/ros2Docker/cmd"
+	"github.com/Abdallemo/ros2Docker/internals/docker"
+	"github.com/Abdallemo/ros2Docker/internals/utils"
+	"github.com/docker/docker/client"
+)
 
 func main() {
-	// _, err := utils.NewConfig("ros2")
-	// if err != nil {
-	// 	log.Fatal("couldne create config folder", err.Error())
-	// }
-	cmd.Execute()
+
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		log.Fatal("unable to connect to docker", err)
+	}
+	dckr := docker.NewDocker(cli)
+	_, err = utils.NewConfig("ros2docker")
+	if err != nil {
+		log.Fatal("unable to setup Program Configurations", err)
+	}
+
+	cmd.Execute(dckr)
 
 }
