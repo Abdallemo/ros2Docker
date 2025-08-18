0b05762dc80004661b74e7624708e36bb80d9597
EngCamo <learn3038it@gmail.com>
2025-08-18 17:47:57 +0800

    added the new command

--- go/cmd/new.go
+++ go/cmd/new.go
@@ -3,19 +3,26 @@ package cmd
 import (
 	"fmt"
 
+	"github.com/Abdallemo/ros2Docker/internals/docker"
 	"github.com/spf13/cobra"
 )
 
 var newCmd = &cobra.Command{
 	Use:   "new",
-	Short: "",
-	Long:  "",
+	Short: "creates a new worksapce for the ros2 project",
+	Long: `[new] command creates a new worksapce for the ros2 project
+	and example:
+	ros2docker new <workspaceName>
+	`,
+	Example: `
+	ros2docker run my_ws --image humble`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("new ros2 Workspace")
+
 	},
 }
 
 func init() {
-	st := newCmd.Flags().BoolP("start", "s", false, "start a new workspace")
-	fmt.Println("value chosen ", &st)
+	rootCmd.AddCommand(newCmd)
+	newCmd.Flags().String("image", docker.Humble, "provide the ros2 version you need for this worksapce/project")
 }
