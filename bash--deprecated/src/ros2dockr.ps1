0624bd4d3adbb7d89d36949f00ef2f815dfa88e8
EngCamo <learn3038it@gmail.com>
2025-08-19 00:32:00 +0800

    moved  --deprecated

---
+++ bash--deprecated/src/ros2dockr.ps1
@@ -0,0 +1,67 @@
+$requiredFiles = @("compose.yaml", "Dockerfile")
+$baseUrl = "https://raw.githubusercontent.com/xaatim/ROS2-Docker-Launcher/main/bash--deprecated/src"
+$docDir = "$env:USERPROFILE\.ros2docker"
+New-Item -ItemType Directory -Force -Path $docDir | Out-Null
+$env:PWD = Get-Location
+Write-Host "Checking for required files..."
+foreach ($file in $requiredFiles) {
+    if (Test-Path "$docDir\$file") {
+        Write-Host " [ok] Found: $file"
+    }
+    else {
+        Write-Host "Missing: $file"
+        Write-Host "Downloading $file..."
+        try {
+            Invoke-WebRequest -Uri "$baseUrl/$file" -OutFile "$docDir\$file" -UseBasicParsing
+            Write-Host "Downloaded: $file"
+        }
+        catch {
+            Write-Error "Failed to download $file"
+            exit 1
+        }
+    }
+}
+
+$Command = $args[0]
+
+switch ($Command) {
+    "-clean" {
+
+        Write-Host "Rebuilding and cleaning volumes"
+        if (docker ps -a --format '{{.Names}}' | Select-String -Pattern "^ros2$") {
+            Write-Host "Removing old container 'ros2'"
+            docker rm -f ros2
+        }
+        docker compose -f "$docDir\compose.yaml" down
+        docker compose -f "$docDir\compose.yaml" up --build -d
+    }
+    "-start" {
+        Write-Host " Starting (without rebuild)"
+        docker compose -f "$docDir\compose.yaml" up -d
+    }
+    "-stop" {
+        Write-Host "Stopping services"
+        docker compose -f "$docDir\compose.yaml" down
+    }
+    "-shell" {
+        Write-Host "Entering container shell"
+        docker exec -it ros2 bash
+    }
+    "-h" {
+        Write-Host "`n=== Help Menu ==="
+        Write-Host "  -clean     [*] Rebuild image and restart container"
+        Write-Host "  -start     [>] Start container (without rebuild)"
+        Write-Host "  -stop      [x] Stop and remove container"
+        Write-Host "  -shell     [$] Enter interactive container shell"
+        Write-Host "  -logs      [#] Follow container logs"
+        Write-Host "  -h, --help [?] Show this help message`n"
+    }
+
+    "--help" {
+        & $Command = "-h"
+    }
+
+    default {
+        Write-Host "Usage: ./ros2dock.ps1 [-help | -h]"
+    }
+}
