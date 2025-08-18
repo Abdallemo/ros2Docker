5ad1ed1500bdc56e9ae0d03509237567f76b6c12
EngCamo <learn3038it@gmail.com>
2025-08-19 00:35:44 +0800

    moved go to the root

---
+++ main.go
@@ -0,0 +1,29 @@
+/*
+Copyright © 2025 Abdallemo <learn3038it@gmail.com>
+*/
+package main
+
+import (
+	"log"
+
+	"github.com/Abdallemo/ros2Docker/cmd"
+	"github.com/Abdallemo/ros2Docker/internals/docker"
+	"github.com/Abdallemo/ros2Docker/internals/utils"
+	"github.com/docker/docker/client"
+)
+
+func main() {
+
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		log.Fatal("unable to connect to docker", err)
+	}
+	dckr := docker.NewDocker(cli)
+	globalCfg, err := utils.NewGlobalConfig("ros2docker")
+	if err != nil {
+		log.Fatal("unable to setup Program Configurations", err)
+	}
+
+	cmd.Execute(dckr, *globalCfg)
+
+}
